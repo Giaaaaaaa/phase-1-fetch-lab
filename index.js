d76a27780fc0d9392a3b7fdd6fc8c42d85570815
wenjia <dwgia027@gmail.com>
2022-12-01 15:49:41 +1100

    assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (books) {
+      renderBooks(books);
+    })
 }
 
 function renderBooks(books) {
@@ -12,6 +18,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
